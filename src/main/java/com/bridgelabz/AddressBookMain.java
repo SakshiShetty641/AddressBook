b3fa6c4689e608487d37ad3262e6c834002ad87d
Sakshi <sakshishetty1231997@gmail.com>
2021-09-10 16:22:40 +0530

    Added: Welcome Message to the Program

---
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+/**
+ * Purpose - To create an Address Book System
+ * @author Sakshi Shetty
+ * @version 16.0
+ * @since 2021-09-10
+ */
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Program");
+    }
+}
